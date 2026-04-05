52922cfc174fc34e7eaec59e47528b8bfa97dedd
Luiz Rosa <ldservicoseletricos@gmail.com>
2026-04-05 02:13:52 -0300

    fix: alinhar imports da auth.route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,17 +2,17 @@ import express from "express";
 import {
   googleCallbackController,
   googleStartController,
-  loginController,
-  meController,
-  registerController,
+  loginUser,
+  getMe,
+  registerUser,
 } from "../controllers/auth.controller.js";
 import { requireAuth } from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
-router.post("/register", registerController);
-router.post("/login", loginController);
-router.get("/me", requireAuth, meController);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/me", requireAuth, getMe);
 
 router.get("/google/start", googleStartController);
 router.get("/google/callback", googleCallbackController);
